52c4fd1ac95eae10f1c2eafac24a14e25f7a885d
rvogel <vogel@hallowelt.biz>
2019-05-31 08:31:13 +0200

    Fix CLI installer
    For some reason the `wgAutoloadClasses` are getting cleared in
    `maintenance/install.php` context. As this is a legacy branch not further
    investigations have been made on this.
    
    Loading the classes directly fixes the issue without breaking the WebInstaller.

--- mw-config/overrides/overrides.php
+++ mw-config/overrides/overrides.php
@@ -1,9 +1,9 @@
 <?php
 
-$GLOBALS['wgAutoloadClasses']['BsWebInstaller'] = __DIR__ . '/includes/BsWebInstaller.php';
-$GLOBALS['wgAutoloadClasses']['BsWebInstallerOptions'] = __DIR__ . '/includes/BsWebInstallerOptions.php';
-$GLOBALS['wgAutoloadClasses']['BsWebInstallerOutput'] = __DIR__ . '/includes/BsWebInstallerOutput.php';
-$GLOBALS['wgAutoloadClasses']['BsLocalSettingsGenerator'] = __DIR__ . '/includes/BsLocalSettingsGenerator.php';
+require_once __DIR__ . '/includes/BsWebInstaller.php';
+require_once __DIR__ . '/includes/BsWebInstallerOptions.php';
+require_once __DIR__ . '/includes/BsWebInstallerOutput.php';
+require_once __DIR__ . '/includes/BsLocalSettingsGenerator.php';
 
 $overrides['LocalSettingsGenerator'] = 'BsLocalSettingsGenerator';
 $overrides['WebInstaller'] = 'BsWebInstaller';
