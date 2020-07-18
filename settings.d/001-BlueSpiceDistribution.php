e1607745e4c1b02f403fe8f5fd136d6d5c96ba52
rvogel <vogel@hallowelt.biz>
2020-07-18 11:20:23 +0200

    Add `_bluespice/` and `settings.d/` from `master`

---
+++ settings.d/001-BlueSpiceDistribution.php
@@ -0,0 +1,24 @@
+<?php
+
+wfLoadExtension( 'Arrays' );
+wfLoadExtension( 'CategoryTree' );
+wfLoadExtension( 'DynamicPageList' );
+require_once __DIR__ . "/../extensions/HitCounters/HitCounters.php";
+require_once __DIR__ . "/../extensions/ImageMapEdit/ImageMapEdit.php";
+require_once __DIR__ . "/../extensions/Quiz/Quiz.php";
+wfLoadExtension( 'RSS' );
+wfLoadExtension( 'Echo');
+wfLoadExtension( 'TitleKey');
+require_once __DIR__ . "/../extensions/EmbedVideo/EmbedVideo.php";
+wfLoadExtension( 'FilterSpecialPages' );
+wfLoadExtension( 'UserMerge' );
+$wgUserMergeProtectedGroups = array();
+$wgUserMergeUnmergeable = array();
+wfLoadExtension( 'Variables' );
+require_once __DIR__ . "/../extensions/EditNotify/EditNotify.php";
+wfLoadExtension( 'BlueSpiceEchoConnector' );
+wfLoadExtension( 'BlueSpiceDistributionConnector' );
+require_once __DIR__ . "/../extensions/BlueSpiceUserMergeConnector/BlueSpiceUserMergeConnector.php";
+wfLoadExtension( 'BlueSpiceEditNotifyConnector' );
+require_once __DIR__ . "/../extensions/UserFunctions/UserFunctions.php";
+require_once __DIR__ . "/../extensions/UrlGetParameters/UrlGetParameters.php";
---
+++ settings.d/005-DefaultSettings.php
@@ -0,0 +1,34 @@
+<?php
+
+$wgLocalTZoffset = date("Z") / 60;
+$wgLocaltimezone = 'Europe/Berlin';
+$wgUrlProtocols[] = 'file://';
+$wgNamespacesWithSubpages[NS_MAIN] = true;
+$wgExternalLinkTarget = '_blank';
+$wgBlockDisablesLogin = true;
+$wgEnableUploads = true;
+
+//Default MediaWiki settings needed for BlueSpice
+$GLOBALS['wgNamespacesWithSubpages'][NS_MAIN] = true;
+$GLOBALS['wgApiFrameOptions'] = 'SAMEORIGIN';
+$GLOBALS['wgRSSUrlWhitelist'] = array(
+	"http://blog.bluespice.com/feed/",
+	"http://blog.hallowelt.com/feed/",
+	"https://blog.bluespice.com/feed/",
+	"https://blog.hallowelt.com/feed/",
+);
+$GLOBALS['wgExternalLinkTarget'] = '_blank';
+$GLOBALS['wgCapitalLinkOverrides'][ NS_FILE ] = false;
+$GLOBALS['wgRestrictDisplayTitle'] = false; //Otherwise only titles that normalize to the same DB key are allowed
+$GLOBALS['wgUrlProtocols'][] = "file://";
+$GLOBALS['wgVerifyMimeType'] = false;
+$GLOBALS['wgAllowJavaUploads'] = true;
+$GLOBALS['wgParserCacheType'] = CACHE_NONE;
+
+/*
+* Allow authentication extensions like "Auth_remoteuser", "SimpleSAMLphp" or
+* "LDAPAuthentication2" to create users.
+*/
+$GLOBALS['wgExtensionFunctions'][] = function() {
+	$GLOBALS['wgGroupPermissions']['*']['autocreateaccount'] = true;
+};
